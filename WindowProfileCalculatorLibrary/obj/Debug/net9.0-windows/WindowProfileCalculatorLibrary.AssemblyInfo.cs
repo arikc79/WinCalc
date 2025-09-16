9f92b0fc3e43a3be4cedb21bcf43492c5eb184d6
glebmelnikov20@gmail.com <glebmelnikov20@gmail.com>
2025-09-16 12:58:27 +0200

    хеш-пароли (PBKDF2) + роли admin/manager + окно логина; SQLite user store

--- WindowProfileCalculatorLibrary/obj/Debug/net9.0-windows/WindowProfileCalculatorLibrary.AssemblyInfo.cs
+++ WindowProfileCalculatorLibrary/obj/Debug/net9.0-windows/WindowProfileCalculatorLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowProfileCalculatorLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46102fcc43bfe0734640a6eab862e17a836d803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2388786f7d057251e975b6d14842fc03376bdac")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowProfileCalculatorLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowProfileCalculatorLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
