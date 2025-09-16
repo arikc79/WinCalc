28c45d26471cb3eb6ff6ea43466ba272d8ea43fd
glebmelnikov20@gmail.com <glebmelnikov20@gmail.com>
2025-09-16 11:58:26 +0200

    Roles

--- WindowPaswoord/obj/Debug/net9.0-windows/WindowPaswoord.AssemblyInfo.cs
+++ WindowPaswoord/obj/Debug/net9.0-windows/WindowPaswoord.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowPaswoord")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747daeda755df1189f393e0e79e47b31b90cba78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9220d4cd125b9e36c27c10aea8afa9e28647f38")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowPaswoord")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowPaswoord")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
