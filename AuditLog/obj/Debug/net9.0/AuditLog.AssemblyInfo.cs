e1e33e79e4f015a7a239ed7e76ff504c03c02d5c
glebmelnikov20@gmail.com <glebmelnikov20@gmail.com>
2025-12-18 19:08:06 +0100

    CenterScreen

--- AuditLog/obj/Debug/net9.0/AuditLog.AssemblyInfo.cs
+++ AuditLog/obj/Debug/net9.0/AuditLog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuditLog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae48be9642aff9ad69d907706bc2e4b04c0039ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca4ccb63505a541706d98e0f2fd5f082954fce2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuditLog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuditLog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
